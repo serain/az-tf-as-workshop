9610b2612e9140876aea74e6102b8102cc1af1e0
Alexandre Kaskasoli <alexandre.kaskasoli@mwrinfosecurity.com>
2018-12-19 19:04:40 +0000

    update

--- build.py
+++ build.py
@@ -7,17 +7,11 @@ TEMPLATE = open('template/slideshow.html.j2').read()
 
 
 sections = []
-
-walked = os.walk('sections')
-next(walked)
-
-print(walker)
+walked = sorted(list(os.walk('sections')), key=lambda tup: tup[0])
 
 for folder in walked:
     files = folder[2]
     for _file in files:
-        print(folder[0], _file)
-        continue
         section = open(f'{folder[0]}/{_file}').read()
     
         if _file != '00-housekeeping.md' and not section.startswith('---'):
